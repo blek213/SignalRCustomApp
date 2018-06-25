3eb80cbe1c70e42c0e896f7f66ab94ca4ad91fc1
Andrew Romanuk <blek213@gmail.com>
2018-06-25 11:13:52 +0300

    simple webchat implementations

---
+++ SignalRCustomApp/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCustomApp
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string username, string message)
+        {
+            await this.Clients.All.SendAsync("Send", username, message);
+        }
+
+    }
+}
