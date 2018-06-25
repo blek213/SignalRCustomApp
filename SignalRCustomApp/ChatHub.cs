e540bfcae052ff843e58086edd77168c6b5f4266
Andrew Romanuk <blek213@gmail.com>
2018-06-25 11:33:17 +0300

    firstcommit

--- SignalRCustomApp/ChatHub.cs
+++ SignalRCustomApp/ChatHub.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +10,29 @@ namespace SignalRCustomApp
 {
     public class ChatHub : Hub
     {
+    //    private IHostingEnvironment _hostingEnvironment;
+
+        static object locker = new object();
+
+
+        //public ChatHub(IHostingEnvironment environment)
+        //{
+        //    _hostingEnvironment = environment;
+        //}
+
         public async Task Send(string username, string message)
         {
+            //lock (locker)
+            //{
+            //   // var path = Path.Combine(_hostingEnvironment.WebRootPath, "Sample.PNG");
+
+            //    FileStream file1 = new FileStream(("~/content/messages.txt"), FileMode.Append);
+            //    StreamWriter writer = new StreamWriter(file1);
+
+            //    writer.Write(username + ":" + message + "\r");
+            //    writer.Close();
+            //}
+
             await this.Clients.All.SendAsync("Send", username, message);
         }
 
